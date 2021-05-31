bd390ba27fe09b142618a1fd2c2057ca93e52afd
hscottvo <hscottvo@gmail.com>
2021-05-30 22:01:23 -0700

    finalize title melody

--- source/main.c
+++ source/main.c
@@ -58,7 +58,7 @@ unsigned char title_melody_size = 96;
 unsigned char title_melody[96] = {a_2, a_1, e_1, a_1, a_2, b_2, c_2, c_2, c_2, c_2, b_2, a_2, 
                                 g_1, g_1, g_1, a_2, g_1, d_1, e_1, e_1, e_1, e_1, e_1, e_1,
                                 a_2, a_1, e_1, a_1, a_2, b_2, c_2, c_2, c_2, b_2, c_2, c_2,
-                                d_2, d_2, c_2, c_2, d_2, g_2, e_2, e_2, e_2, e_2, c_2, b_2,
+                                d_2, d_2, c_2, c_2, d_2, g_2, e_2, e_2, e_2, e_2, e_2, rest,
                                 a_2, a_1, e_1, a_1, a_2, b_2, c_2, c_2, c_2, c_2, b_2, a_2,
                                 g_1, g_1, g_1, a_2, g_1, d_1, e_1, e_1, e_1, e_1, e_1, e_1,
                                 f_1, g_1, a_2, a_2, a_2, c_2, b_2, b_2, g_1, g_1, e_1, e_1, 
