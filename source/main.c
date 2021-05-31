5e01a597cb91b77227f1d3f3a834caf91c46209f
hscottvo <hscottvo@gmail.com>
2021-05-31 16:04:27 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -233,7 +233,7 @@ int main(void) {
         task2.state = mus_state;
         task3.state = game_state;
         task2.period = melody_period;
-        task2.elapsedTime = task2.period;
+        // task2.elapsedTime = task2.period;
         for(unsigned long i = 0; i < numTasks; i++) {
             if(tasks[i]->elapsedTime == tasks[i]->period) {
                 tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
