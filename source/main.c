76cc563bb858b53d35652d6deeded551cee47c99
hscottvo <hscottvo@gmail.com>
2021-05-30 23:08:33 -0700

    increased gameplay melody tempo

--- source/main.c
+++ source/main.c
@@ -65,7 +65,7 @@ unsigned char title_melody[96] = {a_2, a_1, e_1, a_1, a_2, b_2, c_2, c_2, c_2, c
                                   f_1, g_1, a_2, a_2, a_2, c_2, b_2, b_2, g_1, g_1, e_1, e_1, 
                                   a_2, a_2, a_2, a_2, e_2, d_2, d_flat_2, d_flat_2, d_flat_2, d_flat_2, d_flat_2, rest};
 
-unsigned short gameplay_melody_period = 97; // note = 16th note; 4/4 time
+unsigned short gameplay_melody_period = 83; // note = 16th note; 4/4 time
 unsigned char gameplay_melody_size = 64;
 unsigned char gameplay_melody[64] = {a_flat_1, rest, a_flat_1, rest, rest, rest, a_flat_1, rest, rest, rest, a_flat_1, rest, rest, rest, a_flat_1, rest, 
                                    rest, rest, a_flat_1, rest, rest, rest, a_flat_1, rest, a_2, a_2, a_flat_1, a_flat_1, a_2, a_2, a_flat_1, rest,
